c50b3aa27d8bd924da2f15dd14a232a270afdbf4
Camila Garcia <garciacamilalorena@gmail.com>
2025-07-04 13:27:13 -0300

    Build options modified

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Building Docker image with no caché..."
-if ! sudo docker-compose build --no-cache; then
+if ! docker-compose build --no-cache; then
     echo "Error: Could not build the Docker image"
     exit 1
 fi
 
 echo "Starting Docker Compose..."
-if ! sudo docker-compose up; then
+if ! docker-compose up; then
     echo "Error: Could not start Docker Compose"
     exit 1
 fi
