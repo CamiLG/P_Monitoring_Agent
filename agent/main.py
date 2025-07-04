c9c14620cfd474e348de11f9db0176ba40b428b0
Camila Garcia <garciacamilalorena@gmail.com>
2025-07-04 09:53:07 -0300

    Healthcheck Added

--- agent/main.py
+++ agent/main.py
@@ -6,7 +6,7 @@ from sender import send_data_to_api
 
 # This is a simple monitoring agent that collects availability data from a server and send it to an API.
 
-API_URL = "https://api:5050/api/data" #Replace with the corresponding API URL
+API_URL = "https://3.133.137.56:5050/api/data" #Replace with the corresponding API URL
 
 def job():
     """Job to collect data and send it to the API."""
@@ -22,8 +22,8 @@ def job():
 
 def main():
     """Main function to schedule the job."""
-    schedule.every().day.at("00:00").do(job)  # Schedule the job to run daily at midnight
-    # schedule.every(1).minutes.do(job) # Uncomment for testing every minute
+    # schedule.every().day.at("00:00").do(job)  # Schedule the job to run daily at midnight
+    schedule.every(2).minutes.do(job) # Uncomment for testing every minute
     print("Monitoring agent started. Waiting for the scheduled time...")
     while True:
         schedule.run_pending()
